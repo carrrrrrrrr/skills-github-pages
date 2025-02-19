994afd0f922c186c8ce6ea6a750af2b2cfdf46b5
carrrrrrrrr <169034715+carrrrrrrrr@users.noreply.github.com>
2025-02-19 22:56:49 +0800

    Add files via upload

---
+++ src/components/demo002.vue
@@ -0,0 +1,5 @@
+<template>{{ msg }}
+</template>
+<script setup>
+const msg = '这是我的第二步，嘻嘻'
+</script>
---
+++ src/components/demo_v-text.vue
@@ -0,0 +1,7 @@
+<template>
+    <div v-text="a"></div>
+    <div v-html="a"></div>
+</template>
+<script setup>
+const a = '<span>aaaaaaa</span>'
+</script>
